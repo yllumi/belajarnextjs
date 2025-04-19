5ed3465531beb9c8d00c879b8ccd1c9c1b90d1ab
Toni Haryanto <toha.samba@gmail.com>
2025-04-19 12:29:37 +0700

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>CODEPOLITAN untuk Next.js!</h1>
-      <p>Ini project pertama kita untuk belajar deploy 🚀</p>
+      <h1>CODEPOLITAN untuk Next!</h1>
+      <p>Ini project pertama kita untuk belajar deploy nextJS 🚀</p>
     </div>
   );
 }
